f246669788b326d1d733968cb2f9883eba29ba14
Natalie Dodd <nataliedodd89@gmail.com>
2017-04-14 18:59:37 +0200

    installed axios, add google books api key and built action creater to fetch results from api

--- src/components/search.js
+++ src/components/search.js
@@ -5,7 +5,16 @@ export default class Search extends Component {
   render() {
     return (
         <div className="search-component">
-          Search
+          <div className="row">
+            <div className="col-sm-4 col-sm-offset-4">
+              <div className="input-group">
+                <input type="text" className="form-control" placeholder="Search for a book" />
+                <span className="input-group-btn">
+                  <button className="btn btn-default" type="button">Search</button>
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
     );
   }
--- src/reducers/reducer_results.js
+++ src/reducers/reducer_results.js
@@ -1,5 +1,14 @@
-export default function() {
-  return {
-    test: 'test'
+import { FETCH_BOOK_BY } from '../actions/index';
+import { RESET_SEARCH } from '../actions/index';
+
+const INITIAL_STATE = [];
+
+export default function(state = INITIAL_STATE, action) {
+  switch(action.type) {
+  case FETCH_BOOK_BY:
+    return state = [action.payload.data]
+  case RESET_SEARCH:
+    return state = []
   }
+    return state;
 }
