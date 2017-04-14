fac55deac23df297f3530608ec2416f97319928f
Natalie Dodd <nataliedodd89@gmail.com>
2017-04-14 20:32:16 +0200

    creating input that requests from the google books API on input change

--- src/components/search.js
+++ src/components/search.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import { Component } from 'react';
+import { fetchBooks } from '../actions/index';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-export default class Search extends Component {
+class Search extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value : ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value
+    });
+    this.props.fetchBooks(event.target.value)
+  }
+
   render() {
     return (
         <div className="search-component">
           <div className="row">
             <div className="col-sm-4 col-sm-offset-4">
               <div className="input-group">
-                <input type="text" className="form-control" placeholder="Search for a book" />
-                <span className="input-group-btn">
-                  <button className="btn btn-default" type="button">Search</button>
-                </span>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Search for a book"
+                  onChange={this.handleChange}
+                  value={this.state.value} />
+                  <span className="input-group-btn">
+                   <button className="btn btn-default" type="button">Search</button>
+                 </span>
               </div>
             </div>
           </div>
@@ -19,3 +45,10 @@ export default class Search extends Component {
     );
   }
 }
+
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({fetchBooks}, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(Search);
